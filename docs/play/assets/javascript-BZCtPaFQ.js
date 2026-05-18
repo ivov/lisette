53911561ff45f2fce91f3f9746f23bb65a0931e7
Iván Ovejero <ivov.src@gmail.com>
2026-05-18 23:03:12 +0200

    chore: rebuild playground

--- docs/play/assets/javascript-BZCtPaFQ.js
+++ docs/play/assets/javascript-BZCtPaFQ.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-_qexPlQY.js";import"./editor.main-AvGAzaEQ.js";import"./index-Cf8Y9UD7.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-CuWu7eFN.js";import"./editor.main-BFlp8HJ_.js";import"./index-BwChoxVW.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
