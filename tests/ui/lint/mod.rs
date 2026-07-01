d456334610cd4d5bb2d235d11de3bb02cacdad7d
Iván Ovejero <ivov.src@gmail.com>
2026-07-01 19:31:23 +0200

    fix!: align `Slice.append` with Go semantics

--- tests/ui/lint/mod.rs
+++ tests/ui/lint/mod.rs
@@ -269,6 +269,19 @@ fn main() {
 }
 
 #[test]
+fn discarded_slice_append() {
+    assert_lint_snapshot!(
+        r#"
+fn main() {
+  let items: Slice<int> = []
+  items.append(1)
+  let _ = items
+}
+"#
+    );
+}
+
+#[test]
 fn unused_literal() {
     assert_lint_snapshot!(
         r#"
