d03721ccab62c212117a34f62fc061ae059e200c
Iván Ovejero <ivov.src@gmail.com>
2026-07-18 00:18:49 +0200

    fix: reject `self` on interface methods

--- docs/play/assets/htmlMode-_KpG26-1.js
+++ docs/play/assets/htmlMode-_KpG26-1.js
@@ -1,4 +1,4 @@
-import{m as lt}from"./editor.main-OXsFi7Er.js";import"./index-DncfzAD5.js";/*!-----------------------------------------------------------------------------
+import{m as lt}from"./editor.main-Biq18e__.js";import"./index-CpvHpRPt.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
  * Released under the MIT license
