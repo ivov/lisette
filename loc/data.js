cc6c21219b61d8c76af463911328c00d35c5a651
github-action-benchmark <github@users.noreply.github.com>
2026-07-18 11:36:21 +0000

    add production-loc (customSmallerIsBetter) benchmark result for 616025fb2db664b01e43264e6f7b9880ad79b807

--- loc/data.js
+++ loc/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784327300955,
+  "lastUpdate": 1784374581761,
   "repoUrl": "https://github.com/ivov/lisette",
   "entries": {
     "production-loc": [
@@ -4560,6 +4560,95 @@ window.BENCHMARK_DATA = {
           {
             "name": "diagnostics",
             "value": 6226,
+            "unit": "lines"
+          },
+          {
+            "name": "lsp",
+            "value": 4414,
+            "unit": "lines"
+          },
+          {
+            "name": "format",
+            "value": 2812,
+            "unit": "lines"
+          },
+          {
+            "name": "deps",
+            "value": 1031,
+            "unit": "lines"
+          },
+          {
+            "name": "stdlib",
+            "value": 695,
+            "unit": "lines"
+          },
+          {
+            "name": "bindgen",
+            "value": 6046,
+            "unit": "lines"
+          },
+          {
+            "name": "prelude",
+            "value": 890,
+            "unit": "lines"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ivov.src@gmail.com",
+            "name": "Iván Ovejero",
+            "username": "ivov"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "616025fb2db664b01e43264e6f7b9880ad79b807",
+          "message": "feat: `Slice.make` and `Slice.reserve` (#1026)",
+          "timestamp": "2026-07-18T13:35:59+02:00",
+          "tree_id": "593cc166a7cb0534cc24048f0440c37e40071b44",
+          "url": "https://github.com/ivov/lisette/commit/616025fb2db664b01e43264e6f7b9880ad79b807"
+        },
+        "date": 1784374580760,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "total",
+            "value": 115207,
+            "unit": "lines"
+          },
+          {
+            "name": "emit",
+            "value": 29963,
+            "unit": "lines"
+          },
+          {
+            "name": "semantics",
+            "value": 23128,
+            "unit": "lines"
+          },
+          {
+            "name": "passes",
+            "value": 16621,
+            "unit": "lines"
+          },
+          {
+            "name": "syntax",
+            "value": 13184,
+            "unit": "lines"
+          },
+          {
+            "name": "cli",
+            "value": 10137,
+            "unit": "lines"
+          },
+          {
+            "name": "diagnostics",
+            "value": 6286,
             "unit": "lines"
           },
           {
