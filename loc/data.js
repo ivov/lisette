185850d22ef8ad7905398914426e9c255e97687a
github-action-benchmark <github@users.noreply.github.com>
2026-07-25 15:35:02 +0000

    add production-loc (customSmallerIsBetter) benchmark result for e9a613b5e32f0c4494abae36defd81b76f41751d

--- loc/data.js
+++ loc/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784992008625,
+  "lastUpdate": 1784993702758,
   "repoUrl": "https://github.com/ivov/lisette",
   "entries": {
     "production-loc": [
@@ -10109,6 +10109,95 @@ window.BENCHMARK_DATA = {
           {
             "name": "format",
             "value": 2771,
+            "unit": "lines"
+          },
+          {
+            "name": "deps",
+            "value": 1039,
+            "unit": "lines"
+          },
+          {
+            "name": "stdlib",
+            "value": 695,
+            "unit": "lines"
+          },
+          {
+            "name": "bindgen",
+            "value": 6084,
+            "unit": "lines"
+          },
+          {
+            "name": "prelude",
+            "value": 890,
+            "unit": "lines"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ivov.src@gmail.com",
+            "name": "Iván Ovejero",
+            "username": "ivov"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e9a613b5e32f0c4494abae36defd81b76f41751d",
+          "message": "refactor: unify formatter trivia handling (#1121)",
+          "timestamp": "2026-07-25T17:34:40+02:00",
+          "tree_id": "28ee450c2e59650873831412bec70d578c4df739",
+          "url": "https://github.com/ivov/lisette/commit/e9a613b5e32f0c4494abae36defd81b76f41751d"
+        },
+        "date": 1784993701325,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "total",
+            "value": 117862,
+            "unit": "lines"
+          },
+          {
+            "name": "emit",
+            "value": 30167,
+            "unit": "lines"
+          },
+          {
+            "name": "semantics",
+            "value": 24110,
+            "unit": "lines"
+          },
+          {
+            "name": "passes",
+            "value": 17168,
+            "unit": "lines"
+          },
+          {
+            "name": "syntax",
+            "value": 13556,
+            "unit": "lines"
+          },
+          {
+            "name": "cli",
+            "value": 10548,
+            "unit": "lines"
+          },
+          {
+            "name": "diagnostics",
+            "value": 6421,
+            "unit": "lines"
+          },
+          {
+            "name": "lsp",
+            "value": 4420,
+            "unit": "lines"
+          },
+          {
+            "name": "format",
+            "value": 2764,
             "unit": "lines"
           },
           {
