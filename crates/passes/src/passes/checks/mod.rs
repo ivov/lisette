7e5f8613f3db8931d75c0d897539cda1930333ff
Iván Ovejero <ivov.src@gmail.com>
2026-07-28 20:36:12 +0200

    feat: diagnostic for constant cast overflow

--- crates/passes/src/passes/checks/mod.rs
+++ crates/passes/src/passes/checks/mod.rs
@@ -1,5 +1,6 @@
 pub(crate) mod cast_nan_to_int;
 pub(crate) mod const_naming;
+pub(crate) mod constant_cast_overflow;
 pub(crate) mod decimal_file_mode;
 pub(crate) mod duplicate_bindings;
 pub(crate) mod empty_infinite_loop;
