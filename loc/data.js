625cc83a7393b7bd2f12cc1fbdb5ce5aa712e05e
github-action-benchmark <github@users.noreply.github.com>
2026-08-02 18:44:09 +0000

    add production-loc (customSmallerIsBetter) benchmark result for 8a0ad453596f4bf3f93245b3292747cc977244a6

--- loc/data.js
+++ loc/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785696080711,
+  "lastUpdate": 1785696249293,
   "repoUrl": "https://github.com/ivov/lisette",
   "entries": {
     "production-loc": [
@@ -17565,6 +17565,95 @@ window.BENCHMARK_DATA = {
           {
             "name": "lsp",
             "value": 5368,
+            "unit": "lines"
+          },
+          {
+            "name": "format",
+            "value": 2883,
+            "unit": "lines"
+          },
+          {
+            "name": "deps",
+            "value": 1286,
+            "unit": "lines"
+          },
+          {
+            "name": "stdlib",
+            "value": 695,
+            "unit": "lines"
+          },
+          {
+            "name": "bindgen",
+            "value": 7404,
+            "unit": "lines"
+          },
+          {
+            "name": "prelude",
+            "value": 916,
+            "unit": "lines"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ivov.src@gmail.com",
+            "name": "Iván Ovejero",
+            "username": "ivov"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8a0ad453596f4bf3f93245b3292747cc977244a6",
+          "message": "refactor: shed `tokio` (#1195)",
+          "timestamp": "2026-08-02T20:43:47+02:00",
+          "tree_id": "231a9b7e6e24e479748d1023a8ee216e6b64bf1b",
+          "url": "https://github.com/ivov/lisette/commit/8a0ad453596f4bf3f93245b3292747cc977244a6"
+        },
+        "date": 1785696248083,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "total",
+            "value": 125643,
+            "unit": "lines"
+          },
+          {
+            "name": "emit",
+            "value": 30326,
+            "unit": "lines"
+          },
+          {
+            "name": "semantics",
+            "value": 25488,
+            "unit": "lines"
+          },
+          {
+            "name": "passes",
+            "value": 18536,
+            "unit": "lines"
+          },
+          {
+            "name": "syntax",
+            "value": 13305,
+            "unit": "lines"
+          },
+          {
+            "name": "cli",
+            "value": 11925,
+            "unit": "lines"
+          },
+          {
+            "name": "diagnostics",
+            "value": 7517,
+            "unit": "lines"
+          },
+          {
+            "name": "lsp",
+            "value": 5362,
             "unit": "lines"
           },
           {
