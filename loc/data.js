81b65c86fc0264ec462b955c0e0d4851ea58a2e6
github-action-benchmark <github@users.noreply.github.com>
2026-08-02 18:41:20 +0000

    add production-loc (customSmallerIsBetter) benchmark result for 169e59cbb9cfb93bdbe8f3e73e82b5091b9f1262

--- loc/data.js
+++ loc/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785690424370,
+  "lastUpdate": 1785696080711,
   "repoUrl": "https://github.com/ivov/lisette",
   "entries": {
     "production-loc": [
@@ -17456,6 +17456,95 @@ window.BENCHMARK_DATA = {
           {
             "name": "passes",
             "value": 18464,
+            "unit": "lines"
+          },
+          {
+            "name": "syntax",
+            "value": 13305,
+            "unit": "lines"
+          },
+          {
+            "name": "cli",
+            "value": 11932,
+            "unit": "lines"
+          },
+          {
+            "name": "diagnostics",
+            "value": 7517,
+            "unit": "lines"
+          },
+          {
+            "name": "lsp",
+            "value": 5368,
+            "unit": "lines"
+          },
+          {
+            "name": "format",
+            "value": 2883,
+            "unit": "lines"
+          },
+          {
+            "name": "deps",
+            "value": 1286,
+            "unit": "lines"
+          },
+          {
+            "name": "stdlib",
+            "value": 695,
+            "unit": "lines"
+          },
+          {
+            "name": "bindgen",
+            "value": 7404,
+            "unit": "lines"
+          },
+          {
+            "name": "prelude",
+            "value": 916,
+            "unit": "lines"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ivov.src@gmail.com",
+            "name": "Iván Ovejero",
+            "username": "ivov"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "169e59cbb9cfb93bdbe8f3e73e82b5091b9f1262",
+          "message": "refactor: simplify `passes` crate data flow (#1194)",
+          "timestamp": "2026-08-02T20:40:46+02:00",
+          "tree_id": "ae40af76a62865d8ab59841b26365b304cda12d5",
+          "url": "https://github.com/ivov/lisette/commit/169e59cbb9cfb93bdbe8f3e73e82b5091b9f1262"
+        },
+        "date": 1785696079479,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "total",
+            "value": 125656,
+            "unit": "lines"
+          },
+          {
+            "name": "emit",
+            "value": 30326,
+            "unit": "lines"
+          },
+          {
+            "name": "semantics",
+            "value": 25488,
+            "unit": "lines"
+          },
+          {
+            "name": "passes",
+            "value": 18536,
             "unit": "lines"
           },
           {
