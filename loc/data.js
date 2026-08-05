8c741eaac31af29c3f3a17ccf8d05c26fa340ffb
github-action-benchmark <github@users.noreply.github.com>
2026-08-05 16:17:40 +0000

    add production-loc (customSmallerIsBetter) benchmark result for c6917d2fc4740fd5f5847bc1f51cbc3a48129bab

--- loc/data.js
+++ loc/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785881941041,
+  "lastUpdate": 1785946660315,
   "repoUrl": "https://github.com/ivov/lisette",
   "entries": {
     "production-loc": [
@@ -19152,6 +19152,95 @@ window.BENCHMARK_DATA = {
           {
             "name": "syntax",
             "value": 13518,
+            "unit": "lines"
+          },
+          {
+            "name": "cli",
+            "value": 12354,
+            "unit": "lines"
+          },
+          {
+            "name": "diagnostics",
+            "value": 7593,
+            "unit": "lines"
+          },
+          {
+            "name": "lsp",
+            "value": 5414,
+            "unit": "lines"
+          },
+          {
+            "name": "format",
+            "value": 2883,
+            "unit": "lines"
+          },
+          {
+            "name": "deps",
+            "value": 1286,
+            "unit": "lines"
+          },
+          {
+            "name": "stdlib",
+            "value": 695,
+            "unit": "lines"
+          },
+          {
+            "name": "bindgen",
+            "value": 7416,
+            "unit": "lines"
+          },
+          {
+            "name": "prelude",
+            "value": 916,
+            "unit": "lines"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ivov.src@gmail.com",
+            "name": "Iván Ovejero",
+            "username": "ivov"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c6917d2fc4740fd5f5847bc1f51cbc3a48129bab",
+          "message": "refactor: move brace scanning into lexer (#1216)",
+          "timestamp": "2026-08-05T18:17:14+02:00",
+          "tree_id": "97aa4d710716d03c7078ef723e0980195f9b548b",
+          "url": "https://github.com/ivov/lisette/commit/c6917d2fc4740fd5f5847bc1f51cbc3a48129bab"
+        },
+        "date": 1785946658848,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "total",
+            "value": 126746,
+            "unit": "lines"
+          },
+          {
+            "name": "emit",
+            "value": 30337,
+            "unit": "lines"
+          },
+          {
+            "name": "semantics",
+            "value": 25738,
+            "unit": "lines"
+          },
+          {
+            "name": "passes",
+            "value": 18559,
+            "unit": "lines"
+          },
+          {
+            "name": "syntax",
+            "value": 13555,
             "unit": "lines"
           },
           {
