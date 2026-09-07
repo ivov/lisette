11ae08a15a90dff39ffb2465c43870c46b6a3c68
github-action-benchmark <github@users.noreply.github.com>
2026-09-07 19:21:46 +0000

    add production-loc (customSmallerIsBetter) benchmark result for 839e13284d6114ea4b133952a3c99cea877e7463

--- loc/data.js
+++ loc/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788808284850,
+  "lastUpdate": 1788808906818,
   "repoUrl": "https://github.com/ivov/lisette",
   "entries": {
     "production-loc": [
@@ -34712,6 +34712,95 @@ window.BENCHMARK_DATA = {
           {
             "name": "emit",
             "value": 33319,
+            "unit": "lines"
+          },
+          {
+            "name": "semantics",
+            "value": 28190,
+            "unit": "lines"
+          },
+          {
+            "name": "passes",
+            "value": 19300,
+            "unit": "lines"
+          },
+          {
+            "name": "syntax",
+            "value": 15235,
+            "unit": "lines"
+          },
+          {
+            "name": "cli",
+            "value": 13959,
+            "unit": "lines"
+          },
+          {
+            "name": "diagnostics",
+            "value": 8855,
+            "unit": "lines"
+          },
+          {
+            "name": "lsp",
+            "value": 6285,
+            "unit": "lines"
+          },
+          {
+            "name": "format",
+            "value": 2922,
+            "unit": "lines"
+          },
+          {
+            "name": "deps",
+            "value": 1882,
+            "unit": "lines"
+          },
+          {
+            "name": "stdlib",
+            "value": 294,
+            "unit": "lines"
+          },
+          {
+            "name": "bindgen",
+            "value": 10050,
+            "unit": "lines"
+          },
+          {
+            "name": "prelude",
+            "value": 919,
+            "unit": "lines"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ivov.src@gmail.com",
+            "name": "Iván Ovejero",
+            "username": "ivov"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "839e13284d6114ea4b133952a3c99cea877e7463",
+          "message": "feat: idiomatic Go for `unwrap_or` and `map_or` on Go calls (#1423)",
+          "timestamp": "2026-09-07T21:21:20+02:00",
+          "tree_id": "150266dcd83f268aa5823d0cf13f8e87b856f7b0",
+          "url": "https://github.com/ivov/lisette/commit/839e13284d6114ea4b133952a3c99cea877e7463"
+        },
+        "date": 1788808904163,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "total",
+            "value": 141471,
+            "unit": "lines"
+          },
+          {
+            "name": "emit",
+            "value": 33580,
             "unit": "lines"
           },
           {
