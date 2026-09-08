83ff58d3c72e10928534a56ba81da315a3966e98
github-action-benchmark <github@users.noreply.github.com>
2026-09-08 17:24:17 +0000

    add production-loc (customSmallerIsBetter) benchmark result for 1a00f7be0348fa54e700b3ee0d1b83df2cc64a76

--- loc/data.js
+++ loc/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1788867076987,
+  "lastUpdate": 1788888257743,
   "repoUrl": "https://github.com/ivov/lisette",
   "entries": {
     "production-loc": [
@@ -35780,6 +35780,95 @@ window.BENCHMARK_DATA = {
           {
             "name": "emit",
             "value": 35734,
+            "unit": "lines"
+          },
+          {
+            "name": "semantics",
+            "value": 28190,
+            "unit": "lines"
+          },
+          {
+            "name": "passes",
+            "value": 19300,
+            "unit": "lines"
+          },
+          {
+            "name": "syntax",
+            "value": 15235,
+            "unit": "lines"
+          },
+          {
+            "name": "cli",
+            "value": 13959,
+            "unit": "lines"
+          },
+          {
+            "name": "diagnostics",
+            "value": 8855,
+            "unit": "lines"
+          },
+          {
+            "name": "lsp",
+            "value": 6285,
+            "unit": "lines"
+          },
+          {
+            "name": "format",
+            "value": 2922,
+            "unit": "lines"
+          },
+          {
+            "name": "deps",
+            "value": 1882,
+            "unit": "lines"
+          },
+          {
+            "name": "stdlib",
+            "value": 294,
+            "unit": "lines"
+          },
+          {
+            "name": "bindgen",
+            "value": 10050,
+            "unit": "lines"
+          },
+          {
+            "name": "prelude",
+            "value": 919,
+            "unit": "lines"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ivov.src@gmail.com",
+            "name": "Iván Ovejero",
+            "username": "ivov"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1a00f7be0348fa54e700b3ee0d1b83df2cc64a76",
+          "message": "refactor: lower pattern subjects and labels to `GoExpressionNode` (#1435)",
+          "timestamp": "2026-09-08T19:23:50+02:00",
+          "tree_id": "6b9a5ec68f45ce86baf1249e5eee94fe88ad4578",
+          "url": "https://github.com/ivov/lisette/commit/1a00f7be0348fa54e700b3ee0d1b83df2cc64a76"
+        },
+        "date": 1788888255757,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "total",
+            "value": 143293,
+            "unit": "lines"
+          },
+          {
+            "name": "emit",
+            "value": 35402,
             "unit": "lines"
           },
           {
