10a65ff9a5f66f71df2ed8b79900065140eaa6ab
github-action-benchmark <github@users.noreply.github.com>
2026-09-18 10:44:55 +0000

    add production-loc (customSmallerIsBetter) benchmark result for 749bbceb0e28718192de0007ced2dbbfa372ebf8

--- loc/data.js
+++ loc/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789727177559,
+  "lastUpdate": 1789728295047,
   "repoUrl": "https://github.com/ivov/lisette",
   "entries": {
     "production-loc": [
@@ -36403,6 +36403,95 @@ window.BENCHMARK_DATA = {
           {
             "name": "emit",
             "value": 35112,
+            "unit": "lines"
+          },
+          {
+            "name": "semantics",
+            "value": 28427,
+            "unit": "lines"
+          },
+          {
+            "name": "passes",
+            "value": 19344,
+            "unit": "lines"
+          },
+          {
+            "name": "syntax",
+            "value": 15236,
+            "unit": "lines"
+          },
+          {
+            "name": "cli",
+            "value": 13959,
+            "unit": "lines"
+          },
+          {
+            "name": "diagnostics",
+            "value": 9004,
+            "unit": "lines"
+          },
+          {
+            "name": "lsp",
+            "value": 6285,
+            "unit": "lines"
+          },
+          {
+            "name": "format",
+            "value": 2922,
+            "unit": "lines"
+          },
+          {
+            "name": "deps",
+            "value": 1882,
+            "unit": "lines"
+          },
+          {
+            "name": "stdlib",
+            "value": 294,
+            "unit": "lines"
+          },
+          {
+            "name": "bindgen",
+            "value": 10050,
+            "unit": "lines"
+          },
+          {
+            "name": "prelude",
+            "value": 919,
+            "unit": "lines"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ivov.src@gmail.com",
+            "name": "Iván Ovejero",
+            "username": "ivov"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "749bbceb0e28718192de0007ced2dbbfa372ebf8",
+          "message": "fix: return `Option` as one value from generic interface methods (#1443)",
+          "timestamp": "2026-09-18T12:44:33+02:00",
+          "tree_id": "76a501cc75b68b3e45ab6b555bb5296422fbb03e",
+          "url": "https://github.com/ivov/lisette/commit/749bbceb0e28718192de0007ced2dbbfa372ebf8"
+        },
+        "date": 1789728293742,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "total",
+            "value": 143568,
+            "unit": "lines"
+          },
+          {
+            "name": "emit",
+            "value": 35246,
             "unit": "lines"
           },
           {
