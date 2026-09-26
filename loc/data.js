a8a94ef1833a981e1b26c062d7b9c1602b22a05f
github-action-benchmark <github@users.noreply.github.com>
2026-09-26 16:19:56 +0000

    add production-loc (customSmallerIsBetter) benchmark result for c17f8a82665144d9343a382248114d8b26c96e2c

--- loc/data.js
+++ loc/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1790432089533,
+  "lastUpdate": 1790439596224,
   "repoUrl": "https://github.com/ivov/lisette",
   "entries": {
     "production-loc": [
@@ -39340,6 +39340,95 @@ window.BENCHMARK_DATA = {
           {
             "name": "emit",
             "value": 36958,
+            "unit": "lines"
+          },
+          {
+            "name": "semantics",
+            "value": 28431,
+            "unit": "lines"
+          },
+          {
+            "name": "passes",
+            "value": 19344,
+            "unit": "lines"
+          },
+          {
+            "name": "syntax",
+            "value": 15262,
+            "unit": "lines"
+          },
+          {
+            "name": "cli",
+            "value": 13959,
+            "unit": "lines"
+          },
+          {
+            "name": "diagnostics",
+            "value": 9004,
+            "unit": "lines"
+          },
+          {
+            "name": "lsp",
+            "value": 6383,
+            "unit": "lines"
+          },
+          {
+            "name": "format",
+            "value": 2944,
+            "unit": "lines"
+          },
+          {
+            "name": "deps",
+            "value": 1882,
+            "unit": "lines"
+          },
+          {
+            "name": "stdlib",
+            "value": 294,
+            "unit": "lines"
+          },
+          {
+            "name": "bindgen",
+            "value": 10050,
+            "unit": "lines"
+          },
+          {
+            "name": "prelude",
+            "value": 919,
+            "unit": "lines"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ivov.src@gmail.com",
+            "name": "Iván Ovejero",
+            "username": "ivov"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c17f8a82665144d9343a382248114d8b26c96e2c",
+          "message": "refactor: unify bindings and argument lowering (#1480)",
+          "timestamp": "2026-09-26T18:19:31+02:00",
+          "tree_id": "6b329e51ed47db1d346ac12ebc80e79fcaad7dcc",
+          "url": "https://github.com/ivov/lisette/commit/c17f8a82665144d9343a382248114d8b26c96e2c"
+        },
+        "date": 1790439594400,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "total",
+            "value": 145447,
+            "unit": "lines"
+          },
+          {
+            "name": "emit",
+            "value": 36975,
             "unit": "lines"
           },
           {
